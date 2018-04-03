491c9f3616ce5ed4e68f30dcc4c121218d4925d0
Judy Zhang <qianzhiz@usc.edu>
2018-04-02 23:31:00 -0700

    more backend, users and player classes

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -4,5 +4,27 @@ public class Player {
 	private int _wins;
 	private int _losses;
 	
+	public Player() {
+		
+	}
+
+	public int get_wins() {
+		return _wins;
+	}
+
+	public void set_wins(int _wins) {
+		this._wins = _wins;
+	}
+
+	public int get_losses() {
+		return _losses;
+	}
+
+	public void set_losses(int _losses) {
+		this._losses = _losses;
+	}
+	
+	
+	
 }
 
