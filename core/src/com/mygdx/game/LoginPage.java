37b2d647d05e64741965ab778228428b3ce7802f
Soojung Choi <crystasc@usc.edu>
2018-04-04 17:46:56 -0700

    Front end start

--- core/src/com/mygdx/game/LoginPage.java
+++ core/src/com/mygdx/game/LoginPage.java
@@ -5,13 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class LoginPage extends ApplicationAdapter {
+	Stage stage;
 	SpriteBatch batch;
+	TextButton loginButton;
+	TextButtonStyle loginStyle;
 	
 	@Override
 	public void create () {
+		stage = new Stage();
 		batch = new SpriteBatch();
+		loginStyle = new TextButtonStyle();
+		loginButton = new TextButton("LOGIN", loginStyle);
+		stage.addActor(loginButton);
 	}
 
 	@Override
@@ -20,6 +30,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.end();
+		stage.addActor(loginButton);
 	}
 	
 	@Override
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.LoginPage;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		config.height = 1920;
+		config.width = 3072;
+		new LwjglApplication(new LoginPage(), config);
 	}
 }
