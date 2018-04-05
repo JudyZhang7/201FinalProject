bf60e781cedb38ecbd83df41fc93dbfc8d7217a8
Soojung Choi <crystasc@usc.edu>
2018-04-04 21:32:07 -0700

    Stuff

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,11 +10,14 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
+	AssetManager libGDXAssetManager;
+	
 	private SpriteBatch batch;
 	private BitmapFont font;
 	
 	@Override
 	public void create () {
+		libGDXAssetManager = new AssetManager();
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
