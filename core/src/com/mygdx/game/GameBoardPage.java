3fe3034724d388f1e10d307790aa9a60ac4745ec
Soojung Choi <crystasc@usc.edu>
2018-04-07 15:42:42 -0700

    Working on game board

--- core/src/com/mygdx/game/GameBoardPage.java
+++ core/src/com/mygdx/game/GameBoardPage.java
@@ -4,14 +4,19 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class GameBoardPage extends ApplicationAdapter {
-	SpriteBatch batch;
+	private SpriteBatch batch;
+	private Stage stage;
+	private BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		stage = new Stage();
 	}
 
 	@Override
