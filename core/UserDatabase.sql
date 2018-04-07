0f28dde577fccc086895ece6d32978055bffcbb1
Judy Zhang <qianzhiz@usc.edu>
2018-04-07 15:18:22 -0700

    sql

--- core/UserDatabase.sql
+++ core/UserDatabase.sql
@@ -7,14 +7,14 @@ USE ProjectUserDatabase;
 CREATE TABLE ProjectUserTable
 (
 	userID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     userPassword VARCHAR(50) NOT NULL,
     userLevel INT(2) NOT NULL, -- Capped at lvl 99
     userWins INT(4) NOT NULL, -- Capped at 9999
     userLosses INT(4) NOT NULL, -- Capped at 9999
     deck longblob NULL -- null lets you leave off the name
     
-);
+);Page
 
 -- Insert one user to the table to start off
 
