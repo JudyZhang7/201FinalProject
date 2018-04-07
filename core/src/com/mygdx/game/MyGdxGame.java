1523e8c9b3b545b0855fbf596cc0e969082b4063
Judy Zhang <qianzhiz@usc.edu>
2018-04-07 15:38:26 -0700

    login

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,7 +2,6 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.AssetLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,16 +14,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 public class MyGdxGame extends ApplicationAdapter {
-	AssetManager libGDXAssetManager;
-	
 	private SpriteBatch batch;
 	private BitmapFont font;
 	private Stage stage;
 	
 	@Override
 	public void create () {
-		libGDXAssetManager = new AssetManager();
-
 		this.stage = new Stage();
 
 		batch = new SpriteBatch();
