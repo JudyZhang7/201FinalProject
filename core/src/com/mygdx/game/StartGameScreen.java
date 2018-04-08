80c47fe8d47f04fac1ef46199e820da7c6469a22
Ryan Zubery <ryan.zubery@gmail.com>
2018-04-07 20:26:15 -0700

    Done with skeleton of StartGamePage

--- core/src/com/mygdx/game/StartGameScreen.java
+++ core/src/com/mygdx/game/StartGameScreen.java
@@ -4,17 +4,23 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class StartGameScreen implements Screen {
-	Stage stage;
-	Game game;
+	private Stage stage;
+	private Game game;
+	private BitmapFont font;
+	private SpriteBatch batch;
 	
 	public StartGameScreen(Game game) {
 		this.game = game;
 		stage = new Stage();
+		batch = new SpriteBatch();
+		font = new BitmapFont();
 		Gdx.input.setInputProcessor(stage);
 		
 		//Placeholder skins
@@ -30,9 +36,16 @@ public class StartGameScreen implements Screen {
 		btnViewProfile.setPosition(950, 500);
 		btnViewProfile.setSize(225, 75);
 		
+		//Start button
+		TextButton btnStart = new TextButton("Start", textSkin);
+		btnStart.setPosition(550, 350);
+		btnStart.setSize(200, 50);
+		
+		//
 		
 		stage.addActor(btnViewDeck);
 		stage.addActor(btnViewProfile);
+		stage.addActor(btnStart);
 	}
 	
 	@Override
@@ -48,7 +61,10 @@ public class StartGameScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act(delta);
 		stage.draw();
-		
+		batch.begin();
+		font.draw(batch, "Start a New Game", 550, 675);
+		font.draw(batch, "Players looking for games", 75, 600);
+		batch.end();
 	}
 
 	@Override
