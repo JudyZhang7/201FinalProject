0e6028103d35300b0626525817f84faa09ce7d80
Judy Zhang <qianzhiz@usc.edu>
2018-04-07 21:33:50 -0700

    sql

--- core/UserDatabase.sql
+++ core/UserDatabase.sql
@@ -6,15 +6,27 @@ USE ProjectUserDatabase;
 
 CREATE TABLE ProjectUserTable
 (
-	userID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     username VARCHAR(50) NOT NULL UNIQUE,
     userPassword VARCHAR(50) NOT NULL,
     userLevel INT(2) NOT NULL, -- Capped at lvl 99
     userWins INT(4) NOT NULL, -- Capped at 9999
     userLosses INT(4) NOT NULL, -- Capped at 9999
-    deck longblob NULL -- null lets you leave off the name
     
-);Page
+    serialized_id_player int(11) NOT NULL auto_increment,
+	serialized_object_player blob NOT NULL, -- null lets you leave off the name
+    PRIMARY KEY (serialized_id_player),
+    
+    serialized_id_decks int(11) NOT NULL auto_increment,
+	serialized_object_decks blob NOT NULL, -- null lets you leave off the name
+    PRIMARY KEY (serialized_id_decks)
+); 
+
+CREATE TABLE TEST
+(
+	serialized_id_player int(11) NOT NULL auto_increment,
+	serialized_object_player blob NOT NULL, -- null lets you leave off the name
+    PRIMARY KEY (serialized_id_player)
+);
 
 -- Insert one user to the table to start off
 
