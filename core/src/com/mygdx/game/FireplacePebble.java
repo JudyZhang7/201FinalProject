0822c1ddd71028ccb9295240a336d48e569681b6
Raymond Andriejanssen <andrieja@usc.edu>
2018-04-07 22:38:42 -0700

    edited fpplace bebbel

--- core/src/com/mygdx/game/FireplacePebble.java
+++ core/src/com/mygdx/game/FireplacePebble.java
@@ -2,11 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 
+import user.AccessSQLDatabase;
+
 public class FireplacePebble extends Game{
 	
 	@Override
 	public void create() {
 		Assets.load();
+		LoginScreen lScreen = new LoginScreen(this);
+		AccessSQLDatabase acc = new AccessSQLDatabase(lScreen);
 		this.setScreen(new DeckScreen(this));
 		this.setScreen(new GameBoardPage(this));
 	}
