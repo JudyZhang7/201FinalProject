c27774243397e76731dd912a8a501b8125989da0
Judy Zhang <qianzhiz@usc.edu>
2018-04-07 21:49:14 -0700

    sql

--- core/UserDatabase.sql
+++ core/UserDatabase.sql
@@ -10,24 +10,29 @@ CREATE TABLE ProjectUserTable
     userPassword VARCHAR(50) NOT NULL,
     userLevel INT(2) NOT NULL, -- Capped at lvl 99
     userWins INT(4) NOT NULL, -- Capped at 9999
-    userLosses INT(4) NOT NULL, -- Capped at 9999
-    
-    serialized_id_player int(11) NOT NULL auto_increment,
+    userLosses INT(4) NOT NULL -- Capped at 9999
+
+); 
+
+CREATE TABLE PlayerObject
+(
+    username VARCHAR(50) NOT NULL UNIQUE,
+	serialized_id_player int(11) NOT NULL auto_increment,
 	serialized_object_player blob NOT NULL, -- null lets you leave off the name
     PRIMARY KEY (serialized_id_player),
     
-    serialized_id_decks int(11) NOT NULL auto_increment,
-	serialized_object_decks blob NOT NULL, -- null lets you leave off the name
-    PRIMARY KEY (serialized_id_decks)
-); 
-
-CREATE TABLE TEST
-(
-	serialized_id_player int(11) NOT NULL auto_increment,
-	serialized_object_player blob NOT NULL, -- null lets you leave off the name
-    PRIMARY KEY (serialized_id_player)
+    FOREIGN KEY fk1(username) REFERENCES ProjectUserTable(username)
 );
 
+CREATE TABLE DeckObject
+(
+    username VARCHAR(50) NOT NULL UNIQUE,
+	serialized_id_deck int(11) NOT NULL auto_increment,
+	serialized_object_deck blob NOT NULL, -- null lets you leave off the name
+    PRIMARY KEY (serialized_id_deck),
+    
+    FOREIGN KEY fk1(username) REFERENCES ProjectUserTable(username)
+);
 -- Insert one user to the table to start off
 
 INSERT INTO ProjectUserTable (username, userpassword, userLevel, userWins, userLosses)
