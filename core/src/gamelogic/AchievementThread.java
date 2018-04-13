cf0f4df50603344679fed4db412ecba4a852984c
Raymond Andriejanssen <andrieja@usc.edu>
2018-04-12 20:29:08 -0700

    runaway - kanye west

---
+++ core/src/gamelogic/AchievementThread.java
@@ -0,0 +1,15 @@
+package gamelogic;
+
+public class AchievementThread extends Thread
+{
+	/* During the game, Achievement Thread will run and output notifications during the game
+	 * when user successfully finishes quests. A new achievement thread will be created when
+	 * the game is being played, and finishes when the game is over. */
+	
+	// Private Variables
+	private Game game; // Game is where the game is being played
+	
+	// Constructor
+	
+	// Run Method
+}
