277133dda25109f635d26337e6c58d1abab5253f
Simon Chan <schan92@hotmail.com>
2018-04-14 13:28:15 -0700

    action card bool

--- core/src/user/ActionCard.java
+++ core/src/user/ActionCard.java
@@ -11,6 +11,7 @@ public class ActionCard extends Card {
 	private int _mana;
 	private String _actionName;
 	private Sprite _mSprite;
+	private Boolean ACTIONDEAD;
 	
 	private int _damageOpponentDoes = 0;
 	private Action state;
@@ -54,26 +55,37 @@ public class ActionCard extends Card {
 		switch(state) {
 		case Weapon:
 			opponent.set_hp(opponent.get_hp() - _damage);
+			this.ACTIONDEAD = isDead();
 			break;
 		case Shield:
 			// check if opponent attacked you
 			// store damage inflicted to add back to your hp
 			// if yes, add the damage the opponent inflicted back into your hp
 			opponent.set_hp(opponent.get_hp() - _damageOpponentDoes);
+			this.ACTIONDEAD = isDead();
 			break;
 		case Heal:
 			you.set_hp(you.get_hp() + _hpReplenish);
+			this.ACTIONDEAD = isDead();
 			break;
 		case DoubleDamage:
 			opponent.set_hp(opponent.get_hp() - 2 * _damage);
+			this.ACTIONDEAD = isDead();
 			break;
 		case Preparation:
 			you.set_mana(you.get_mana() + _mana);
+			this.ACTIONDEAD = isDead();
 			break;
 		}
 	}
 	
 	public void set_damageOpponentDoes(int damage ) {
 		_damageOpponentDoes = damage;
+	}
+
+	@Override
+	public Boolean isDead() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 }
